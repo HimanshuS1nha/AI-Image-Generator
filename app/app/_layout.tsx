acedb5f1c3b766bbd50ed1701f6cf2f634cdc94e
Himanshu Sinha <ashusinha1904@gmail.com>
2025-09-12 22:19:11 +0530

    Layout file updated

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,12 +1,36 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Text, View } from "react-native";
+import tw from "twrnc";
+import { Entypo } from "@expo/vector-icons";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerTitle: (props) => {
+            return (
+              <View
+                style={tw`flex-row gap-x-3 items-center justify-center`}
+                {...props}
+              >
+                <Entypo name="image" size={24} color="white" />
+                <Text style={tw`text-white font-semibold text-lg`}>
+                  AI Image Generator
+                </Text>
+              </View>
+            );
+          },
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: "#000",
+          },
+          statusBarStyle: "light",
+        }}
+      />
     </QueryClientProvider>
   );
 }
