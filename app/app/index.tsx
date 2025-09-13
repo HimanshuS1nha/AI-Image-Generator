e1209035f6950890952b84f75e5f3c2c487141b6
Himanshu Sinha <ashusinha1904@gmail.com>
2025-09-13 21:03:32 +0530

    UI updated

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, Pressable } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { Feather } from "@expo/vector-icons";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={tw`bg-black flex-1 px-2`}>
+      <View style={tw`flex-1 pb-4`}></View>
+
+      <View style={tw`flex-row gap-x-3.5 items-center`}>
+        <TextInput
+          style={tw`border-b border-b-white flex-1 text-white`}
+          placeholder="Type here..."
+          placeholderTextColor={"#d1d5dc"}
+        />
+
+        <Pressable style={tw`bg-indigo-600 p-1.5 rounded-full`}>
+          <Feather name="send" size={24} color="white" />
+        </Pressable>
+      </View>
     </View>
   );
 }
