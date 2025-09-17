5eca6da77b246ea817393332aa1d10561393976f
Himanshu Sinha <ashusinha1904@gmail.com>
2025-09-17 22:00:56 +0530

    Dark mode enabled by default

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,6 +1,12 @@
+import { useLayoutEffect } from "react";
+import { setTheme } from "@tauri-apps/api/app";
+
 import { Button } from "./components/ui/button";
 
 const App = () => {
+  useLayoutEffect(() => {
+    setTheme("dark");
+  }, []);
   return <Button>Hello</Button>;
 };
 
