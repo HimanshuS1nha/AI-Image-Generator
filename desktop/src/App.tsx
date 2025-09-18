74b369b5e2b92386685748ab377ecf88b047d8b8
Himanshu Sinha <ashusinha1904@gmail.com>
2025-09-18 21:39:07 +0530

    Config section added

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,13 +1,24 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import { setTheme } from "@tauri-apps/api/app";
 
-import { Button } from "./components/ui/button";
+import ConfigSection from "./components/config-section";
+
+import { aspectRatios } from "./constants/aspect-ratios";
 
 const App = () => {
+  const [aspectRatio, setAspectRatio] = useState(aspectRatios[0].value);
+
   useLayoutEffect(() => {
     setTheme("dark");
   }, []);
-  return <Button>Hello</Button>;
+  return (
+    <div className="flex gap-x-4 px-4 mt-4">
+      <ConfigSection
+        aspectRatio={aspectRatio}
+        setAspectRatio={setAspectRatio}
+      />
+    </div>
+  );
 };
 
 export default App;
---
+++ desktop/src/components/config-section.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { cn } from "@/lib/utils";
+
+import { aspectRatios } from "@/constants/aspect-ratios";
+
+const ConfigSection = ({
+  aspectRatio,
+  setAspectRatio,
+}: {
+  aspectRatio: string;
+  setAspectRatio: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  return (
+    <div className="flex flex-col gap-y-4 p-3 rounded-lg w-[25%] border-[0.5px] border-gray-500 bg-[#111311] h-fit">
+      <p>Aspect Ratio</p>
+
+      <div className="flex gap-2 items-center flex-wrap">
+        {aspectRatios.map((ratio) => {
+          return (
+            <div
+              key={ratio.value}
+              className={cn(
+                "px-3 py-1.5 rounded-lg flex items-center justify-center border-[0.5px] border-gray-500 cursor-pointer",
+                ratio.value === aspectRatio ? "bg-primary" : "bg-black"
+              )}
+              onClick={() => setAspectRatio(ratio.value)}
+            >
+              <p className="text-smbunx --bun shadcn@latest add textarea">
+                {ratio.value}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ConfigSection;
