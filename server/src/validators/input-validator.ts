38d5d10e3a97d74e01f30d8a802ddcabe6cd0b04
Himanshu Sinha <ashusinha1904@gmail.com>
2025-09-19 22:11:04 +0530

    Validation logic updated

--- server/src/validators/input-validator.ts
+++ server/src/validators/input-validator.ts
@@ -1,6 +1,12 @@
 import z from "zod";
 
 export const inputValidator = z.object({
-  prompt: z.string({ error: "Prompt is required" }),
-  aspectRatio: z.string({ error: "Aspect Ratio is required" }),
+  prompt: z
+    .string({ error: "Prompt is required" })
+    .trim()
+    .min(1, { error: "Prompt is required" }),
+  aspectRatio: z
+    .string({ error: "Aspect Ratio is required" })
+    .trim()
+    .min(1, { error: "Aspect Ratio is required" }),
 });
