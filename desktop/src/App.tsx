b1ed76f41820a22dc5b8d17ce9736b902167a28e
Himanshu Sinha <ashusinha1904@gmail.com>
2025-09-20 22:15:40 +0530

    Added logic to get and update recent prompts

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -1,8 +1,9 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { setTheme } from "@tauri-apps/api/app";
 import { invoke } from "@tauri-apps/api/core";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { getStore, load } from "@tauri-apps/plugin-store";
 
 import ConfigSection from "./components/config-section";
 import MainSection from "./components/main-section";
@@ -17,6 +18,7 @@ const App = () => {
     src: string;
     aspectRatio: string;
   }>();
+  const [recentPrompts, setRecentPrompts] = useState<string[]>([]);
 
   const { mutate: handleGenerateImage, isPending } = useMutation({
     mutationKey: ["generate-image"],
@@ -28,17 +30,51 @@ const App = () => {
 
       return data as string;
     },
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       setGeneratedImage({ src: data, aspectRatio });
+
+      const newRecentPrompts = [input, ...recentPrompts];
+      if (newRecentPrompts.length > 3) {
+        newRecentPrompts.pop();
+      }
+      setRecentPrompts(newRecentPrompts);
+
+      setInput("");
+
+      const store = await getStore("recent-prompts.json");
+      if (store) {
+        await store.set("recentPrompts", newRecentPrompts);
+        await store.save();
+      }
     },
     onError: (error: string) => {
       toast.error(error);
     },
   });
 
+  const { data } = useQuery({
+    queryKey: ["get-recent-prompts"],
+    queryFn: async () => {
+      const store = await load("recent-prompts.json", {
+        autoSave: false,
+        defaults: {},
+      });
+
+      const data = await store.get<string[]>("recentPrompts");
+
+      return data;
+    },
+  });
+
   useLayoutEffect(() => {
     setTheme("dark");
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      setRecentPrompts(data);
+    }
+  }, [data]);
   return (
     <div className="flex justify-center mt-4 px-4">
       <div className="flex flex-col lg:flex-row gap-4 max-w-[1200px] w-full justify-center">
@@ -55,7 +91,7 @@ const App = () => {
           isPending={isPending}
         />
 
-        <HistorySection />
+        <HistorySection recentPrompts={recentPrompts} />
       </div>
     </div>
   );
--- desktop/src/components/history-section.tsx
+++ desktop/src/components/history-section.tsx
@@ -1,21 +1,20 @@
-const HistorySection = () => {
-  const recentPrompts = [
-    "Pikachu coming out of a pokeball",
-    "A family enjoying at the beach",
-    "A car race competition",
-  ];
+const HistorySection = ({ recentPrompts }: { recentPrompts: string[] }) => {
   return (
-    <div className="flex flex-col gap-y-4 p-3 rounded-lg w-full lg:w-[25%] bg-[#171a17] h-fit">
+    <div className="flex flex-col gap-y-4 p-3 rounded-lg w-full lg:w-[25%] border-[0.5px] border-gray-500 bg-[#111311] h-fit">
       <p>Last 3 Prompts</p>
 
-      <div className="flex flex-col gap-y-2.5">
-        {recentPrompts.map((prompt) => {
-          return (
-            <div key={prompt} className="p-2 rounded-lg bg-[#2d2f2f]">
-              <p className="text-sm">{prompt}</p>
-            </div>
-          );
-        })}
+      <div className="flex flex-col gap-y-2">
+        {recentPrompts.length > 0 ? (
+          recentPrompts.map((prompt) => {
+            return (
+              <div key={prompt} className="p-2.5 rounded-lg bg-[#2d2f2f]">
+                <p className="text-sm">{prompt}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p className="text-sm text-rose-300">No data to show</p>
+        )}
       </div>
     </div>
   );
